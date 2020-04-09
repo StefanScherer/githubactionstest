2741b8388b2719045a19b0bd4e7d8369d0c48161
Stefan Scherer <stefan.scherer@docker.com>
2020-04-09 09:49:45 +0200

    Try secrets

--- .github/actions/action-in-go/main.go
+++ .github/actions/action-in-go/main.go
@@ -7,5 +7,14 @@ func main() {
 	if fruit == "" {
 		githubactions.Fatalf("missing input 'fruit'")
 	}
-	githubactions.AddMask(fruit)
+	accessKey := githubactions.GetInput("AWS_ACCESS_KEY_ID")
+	if accessKey == "" {
+		githubactions.Fatalf("missing input 'AWS_ACCESS_KEY_ID'")
+	}
+	githubactions.AddMask(accessKey)
+	secretKey := githubactions.GetInput("AWS_SECRET_ACCESS_KEY")
+	if secretKey == "" {
+		githubactions.Fatalf("missing input 'AWS_SECRET_ACCESS_KEY'")
+	}
+	githubactions.AddMask(secretKey)
 }
